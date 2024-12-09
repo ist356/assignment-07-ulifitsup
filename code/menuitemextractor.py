7c5359e0acea2f2e595437c7f3ed3da588547e29
UlifiPond <ulifi0110901@gmail.com>
2024-12-09 12:45:03 -0500

    part 1 complete

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -7,13 +7,33 @@ else:
 
 
 def clean_price(price:str) -> float:
-    pass # TODO  replace with code
+    price = price.replace("$", "")
+    price = price.replace(",", "")
+    return float(price)
 
 def clean_scraped_text(scraped_text: str) -> list[str]:
-    pass # TODO  replace with code
+    items = scraped_text.split("\n")
+    cleaned = []
+    for item in items:
+        if item in ['GS',"V","S","P"]:
+            continue
+        if item.startswith("NEW"):
+            continue
+        if len(item.strip()) == 0:
+            continue
+        cleaned.append(item)
+    return cleaned
 
 def extract_menu_item(title:str, scraped_text: str) -> MenuItem:
-    pass # TODO  replace with code
+    cleaned_items = clean_scraped_text(scraped_text)
+    item = MenuItem(category=title, name="", price=0.0, description="")
+    item.name = cleaned_items[0]
+    item.price = clean_price(cleaned_items[1])
+    if len(cleaned_items) > 2:
+        item.description = cleaned_items[2]
+    else:
+        item.description = "No description available."
+    return item
 
 
 
